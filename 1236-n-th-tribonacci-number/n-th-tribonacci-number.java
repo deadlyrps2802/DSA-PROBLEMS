b42f60ccec4a0cac245fc455d330594a633bf459
Rudra Pratap Singh <singhrudra8957@gmail.com>
2026-06-15 16:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.74%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,15 +1,26 @@
 class Solution {
-    static int []dp;
+    // static int []dp;
     public int tribonacci(int n) {
-        dp = new int[n+1];
+        // dp = new int[n+1];
         
-        return trib(n);
+        // return trib(n);
+
+        int[] dp = new int[n+1];
+        if(n==0||n==1) return n;
+        if(n==2) return 1;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
     }
 
-    public int trib(int n){
-        if(n==0 || n==1 ) return n;
-        if(n==2) return 1;
-        if(dp[n]!=0) return dp[n];
-        return dp[n] = trib(n-1)+trib(n-2)+trib(n-3);
-    }
+    // public int trib(int n){
+    //     if(n==0 || n==1 ) return n;
+    //     if(n==2) return 1;
+    //     if(dp[n]!=0) return dp[n];
+    //     return dp[n] = trib(n-1)+trib(n-2)+trib(n-3);
+    // }
 }
