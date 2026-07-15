451aff3b86c96ce0e54b21aff2342255bd846a6d
Rudra Pratap Singh <singhrudra8957@gmail.com>
2026-07-15 12:03:43 +0530

    Time: 1 ms (87.69%) | Memory: 47.3 MB (43.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -2,20 +2,20 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int count = 0;
       int V = isConnected.length;
-       ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+    //    ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
 
-       for(int i = 0; i<V; i++){
-        arr.add(new ArrayList<Integer>());
-       }
+    //    for(int i = 0; i<V; i++){
+    //     arr.add(new ArrayList<Integer>());
+    //    }
 
 
-       for(int i = 0; i<V; i++){
-            for(int j = 0; j<V; j++){
-                if(isConnected[i][j]==1 && i!=j ){
-                    arr.get(i).add(j);
-                }
-            }
-        }
+    //    for(int i = 0; i<V; i++){
+    //         for(int j = 0; j<V; j++){
+    //             if(isConnected[i][j]==1 && i!=j ){
+    //                 arr.get(i).add(j);
+    //             }
+    //         }
+    //     }
        
 
        boolean[] vis = new boolean[V];
@@ -23,18 +23,24 @@ class Solution {
        for(int i = 0; i<V; i++){
         if(!vis[i]){
             count++;
-            dfs(i,arr,vis);
+            dfs(i,isConnected,vis);
         }
        }
       return count;
     }
 
-    public void dfs(int node , ArrayList<ArrayList<Integer>> arr ,boolean[] vis){
+    public void dfs(int node , int[][] isConnected ,boolean[] vis){
         vis[node] = true;
-        for(Integer i : arr.get(node)){
-            if(!vis[i]){
-            dfs(i,arr,vis);
-        }
+        // for(Integer i : arr.get(node)){
+        //     if(!vis[i]){
+        //     dfs(i,arr,vis);
+        // }
+        // }
+
+        for(int neighbour = 0; neighbour<isConnected.length; neighbour++){
+            if(isConnected[node][neighbour] == 1 && !vis[neighbour]){
+                dfs(neighbour,isConnected,vis);
+            }
         }
     }
 }
